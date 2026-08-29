a275475acbb1e34ed60a71fbcfa8277d93761260
Kuldeep <kuldeepkumarrf@gmail.com>
2026-08-30 02:14:58 +0530

    Add all Terraform resources, modules, preprod and Prod environments

---
+++ Infrastructure/Infra29082026/Prod/run_security_checks.ps1
@@ -0,0 +1,24 @@
+# Add paths for security tools
+$env:PATH = "C:\Users\DELL\AppData\Local\Programs\Python\Python312;C:\Users\DELL\AppData\Local\Programs\Python\Python312\Scripts;C:\Devops\Tool\terraform_1.14.6_windows_amd64;" + $env:PATH
+
+Write-Host "==========================================" -ForegroundColor Cyan
+Write-Host " 1. Running Gitleaks (Secret Detection) " -ForegroundColor Cyan
+Write-Host "==========================================" -ForegroundColor Cyan
+gitleaks detect --source . --verbose
+
+Write-Host "`n==========================================" -ForegroundColor Cyan
+Write-Host " 2. Running TFSec (Terraform Security)  " -ForegroundColor Cyan
+Write-Host "==========================================" -ForegroundColor Cyan
+tfsec . --tfvars-file terraform.tfvars
+
+Write-Host "`n==========================================" -ForegroundColor Cyan
+Write-Host " 3. Running TFLint (Terraform Linter)   " -ForegroundColor Cyan
+Write-Host "==========================================" -ForegroundColor Cyan
+if (Get-Command tflint -ErrorAction SilentlyContinue) {
+    tflint
+}
+
+Write-Host "`n==========================================" -ForegroundColor Cyan
+Write-Host " 4. Running Checkov (Policy & Compliance)" -ForegroundColor Cyan
+Write-Host "==========================================" -ForegroundColor Cyan
+& "C:\Users\DELL\AppData\Local\Programs\Python\Python312\python.exe" -m checkov.main -d . --framework terraform
---
+++ Infrastructure/Infra29082026/preprod/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm={
+        source = "hashicorp/azurerm"
+        version="4.81.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    subscription_id="72b0382c-3618-4b40-8fa7-98a41ddca4b8"
+    features{}
+  
+}
